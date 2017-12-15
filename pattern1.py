01b5196a402e7d0f1ac800e2dc551bccdb539400
Abdullah <abdullahmohamed95@stud.cu.edu.eg.com>
2017-12-15 16:34:15 +0200

    first commit

---
+++ pattern1.py
@@ -0,0 +1,9 @@
+from scipy import misc
+from matplotlib import pyplot as plt
+# import tensorflow as tf
+
+
+path_line ="/home/abdullah/PycharmProjects/Pattern/Line_TS/"
+
+image = misc.imread()
+plt.imshow(image)
